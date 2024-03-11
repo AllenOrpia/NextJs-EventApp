8180481c809c12edd06e74f4d5eb9c7268e57bfe
allenOrpia <118657615+allenOrpia@users.noreply.github.com>
2024-03-11 14:10:31 -0700

    added context

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,14 +1,20 @@
 
-import { Fragment } from "react";
+import { Fragment, useContext } from "react";
 import Header from "./Header";
+import NotificationContext from "../../store/notification-context";
 
 const layout = (props) => {
+    const notificationCtx = useContext(NotificationContext);
+
+    const activeNotification = notificationCtx.notification;
+
     return (
         <Fragment>
             <Header />
             <main>
                 {props.children}
             </main>
+            {activeNotification && <p>{activeNotification.title} - {activeNotification.message}</p>}
         </Fragment>
     )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,22 @@
 import '../styles/globals.css'
 import Layout from '../components/layout/layout.js'
 import Head from 'next/head.js'
+import { NotificationContextProvider } from '../store/notification-context.js'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Head>
-        <title>Next Events</title>
-        <meta name='description' content='Find events' />
-        <meta name="viewport" content='initial-scale=1.0, width=device-width' />
-      </Head>
-      <Component {...pageProps} />
-    </Layout>
+    <NotificationContextProvider>
+      <Layout>
+        <Head>
+          <title>Next Events</title>
+          <meta name='description' content='Find events' />
+          <meta name="viewport" content='initial-scale=1.0, width=device-width' />
+        </Head>
+        <Component {...pageProps} />
+      </Layout>
+
+
+    </NotificationContextProvider>
   )
 
 }
